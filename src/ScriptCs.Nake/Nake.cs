b93af6efc31fad4f7c7273a2094020443b42a963
prabirshrestha <mail@prabir.me>
2013-07-18 23:34:04 -0400

    add NakeTask to dict

--- src/ScriptCs.Nake/Nake.cs
+++ src/ScriptCs.Nake/Nake.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using ScriptCs.Contracts;
 
@@ -8,10 +10,13 @@ namespace ScriptCs.Nake
     {
         private string currentNamespace;
         private string currentDesc;
+        private IDictionary<string, NakeTask> tasks;
+
         public Nake()
         {
             this.currentNamespace = null;
             this.currentDesc = null;
+            this.tasks = new Dictionary<string, NakeTask>();
         }
 
         public void desc(string description)
@@ -19,52 +24,111 @@ namespace ScriptCs.Nake
             this.currentDesc = description;
         }
 
-        public void task(string name, Action<NakeTaskParameter> callback)
+        public void task(string name, Action<NakeTaskParameter> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, Func<NakeTaskParameter, Task> callback)
+        public void task(string name, Func<NakeTaskParameter, Task> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string[] deps)
+        public void task(string name, string[] deps, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            task(name, p => { });
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string[] deps, Action<NakeTaskParameter> callback)
+        public void task(string name, string[] deps, Action<NakeTaskParameter> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string deps)
+        public void task(string name, string deps, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            task(name, deps, p => { });
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string deps, Action<NakeTaskParameter> callback)
+        public void task(string name, string deps, Action<NakeTaskParameter> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            task(name, deps.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries), callback);
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, Func<Task> callback)
+        public void task(string name, Func<Task> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string[] deps, Func<NakeTaskParameter, Task> callback)
+        public void task(string name, string[] deps, Func<NakeTaskParameter, Task> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
-        public void task(string name, string deps, Func<NakeTaskParameter, Task> callback)
+        public void task(string name, string deps, Func<NakeTaskParameter, Task> callback, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            task(name, deps.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries), callback);
+            var fullName = getFullTaskName(name);
+            var task = new NakeTask(fullName, deps, callback, this.currentDesc)
+            {
+                FilePath = filePath,
+                LineNumber = lineNumber
+            };
+            this.tasks[fullName] = task;
             this.currentDesc = null;
         }
 
@@ -73,7 +137,7 @@ namespace ScriptCs.Nake
             var originalNs = this.currentNamespace;
 
             this.currentNamespace = string.IsNullOrWhiteSpace(this.currentNamespace)
-                ? ns 
+                ? ns
                 : string.Format("{0}:{1}", this.currentNamespace, ns);
 
             this.currentDesc = null;
---
+++ src/ScriptCs.Nake/NakeTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ScriptCs.Nake
+{
+    class NakeTask
+    {
+        public string FilePath { get; set; }
+
+        public int LineNumber { get; set; }
+
+        public NakeTask(string fullName, Action<NakeTaskParameter> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, Func<NakeTaskParameter, Task> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, string[] deps, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, string[] deps, Action<NakeTaskParameter> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, string callback, string deps)
+        {
+        }
+
+        public NakeTask(string fullName, string deps, Action<NakeTaskParameter> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, Func<Task> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, string[] deps, Func<NakeTaskParameter, Task> callback, string desc)
+        {
+        }
+
+        public NakeTask(string fullName, string deps, Func<NakeTaskParameter, Task> callback, string desc)
+        {
+        }
+    }
+}
