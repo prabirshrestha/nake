aebd9888a6790477d46db624c6d2d80dc342d25d
prabirshrestha <mail@prabir.me>
2013-07-18 23:07:47 -0400

    reset currentDesc to null when done

--- src/ScriptCs.Nake/Nake.cs
+++ src/ScriptCs.Nake/Nake.cs
@@ -6,52 +6,72 @@ namespace ScriptCs.Nake
 {
     public class Nake : IScriptPackContext
     {
+        private string currentNamespace;
+        private string currentDesc;
+
+        public Nake()
+        {
+            this.currentNamespace = null;
+            this.currentDesc = null;
+        }
+
         public void desc(string description)
         {
+            this.currentDesc = description;
         }
 
         public void task(string name, Action<NakeTaskParameter> callback)
         {
+            this.currentDesc = null;
         }
 
         public void task(string name, Func<NakeTaskParameter, Task> callback)
         {
+            this.currentDesc = null;
         }
 
         public void task(string name, string[] deps)
         {
             task(name, p => { });
+            this.currentDesc = null;
         }
 
         public void task(string name, string[] deps, Action<NakeTaskParameter> callback)
         {
+            this.currentDesc = null;
         }
 
         public void task(string name, string deps)
         {
             task(name, deps, p => { });
+            this.currentDesc = null;
         }
 
         public void task(string name, string deps, Action<NakeTaskParameter> callback)
         {
             task(name, deps.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries), callback);
+            this.currentDesc = null;
         }
 
         public void task(string name, Func<Task> callback)
         {
+            this.currentDesc = null;
         }
 
         public void task(string name, string[] deps, Func<NakeTaskParameter, Task> callback)
         {
+            this.currentDesc = null;
         }
 
         public void task(string name, string deps, Func<NakeTaskParameter, Task> callback)
         {
             task(name, deps.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries), callback);
+            this.currentDesc = null;
         }
 
         public void ns(string ns, Action block)
         {
+            this.currentDesc = null;
         }
 
         public int execute(string[] args)
